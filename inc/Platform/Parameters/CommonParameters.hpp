95e9d26fd0ec6b70145dd456e211fe7f7641141e
Ilia Zarka <iliazark@gmail.com>
2025-01-19 13:27:55 +0200

    feat: added script to automagicaly pull parameters from the database

--- inc/Platform/Parameters/CommonParameters.hpp
+++ inc/Platform/Parameters/CommonParameters.hpp
@@ -4,10 +4,10 @@
 #include "PeakSatParameters.hpp"
 
 namespace CommonParameters {
-    inline auto& boardTemperature1 = PeakSatParameters::obcPCBTemperature1;
-    inline auto& boardTemperature2 = PeakSatParameters::obcPCBTemperature2;
-    inline auto& mcuTemperature = PeakSatParameters::obcMCUTemperature;
-    inline auto& time = PeakSatParameters::obcOnBoardTime;
+    inline auto& boardTemperature1 = OBDHParameters::PCBTemperature1;
+    inline auto& boardTemperature2 = OBDHParameters::PCBTemperature2;
+    inline auto& mcuTemperature = OBDHParameters::MCUTemperature;
+    inline auto& time = OBDHParameters::OnBoardTime;
     inline auto& useRTT = PeakSatParameters::obcUseRTT;
     inline auto& useUART = PeakSatParameters::obcUseUART;
     inline auto& useCAN = PeakSatParameters::obcUseCAN;
--- inc/Platform/Parameters/HousekeepingService.hpp
+++ inc/Platform/Parameters/HousekeepingService.hpp
@@ -5,17 +5,16 @@
 #include "Services/HousekeepingService.hpp"
 
 namespace HousekeepingStructures {
-    using namespace PeakSatParameters;
 
-    static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
-        {
-            // OBC Parameters that will be monitored in the campaign
-            {0, 1000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCMCUTemperature, OBCMCUBootCounter, OBCMemoryPartition, OBCMCUSystick, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCMRAMON, OBCUseCAN, OBCUseUART, OBCUseRTT, OBCOnBoardTime}},
-            // OBC Mission Database parameters monitored every 3 sec
-            {1, 3000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCSpacecraftTimeRef, OBCOperationalMode, OBCMemoryPartition, OBCReconfigurationTimer, OBCLastFailedEvent, OBCMCUSystick}},
-            // OBC Mission Database parameters not sampled
-            {2, 5000, false, {OBCMCUInputVoltage, OBCFlashInt, OBCSRAMInt, OBCAvailableMRAM, OBCAvailableNAND, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCMCUFDIR, OBCMCURestartSafeModeThreshold, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCNANDFLASHScrubbingFrequency, OBCRAMScrubbingFrequency, OBCProgramFlashScrubbingFrequency}},
-        }};
+    // static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
+    //     {
+    //         // OBC Parameters that will be monitored in the campaign
+    //         {0, 1000, true, {OBDHParameters::PCBTemperature1ID, OBDHParameters::PCBTemperature2ID, OBDHParameters::MCUTemperatureID, OBDHParameters::MCUBootCounterID, OBCMemoryPartition, OBCMCUSystick, OBDHParameters::CANBUSLoad1ID, OBDHParameters::CANBUSLoad2ID, OBDHParameters::CANBUSActiveID, OBDHParameters::NANDFLASHLCLThresholdID, OBDHParameters::MRAMLCLThresholdID, OBDHParameters::NANDFLASHONID, OBDHParameters::MRAMONID, OBCUseCAN, OBCUseUART, OBCUseRTT, OBDHParameters::OnBoardTimeID}},
+    //         // OBC Mission Database parameters monitored every 3 sec
+    //         {1, 3000, true, {OBDHParameters::PCBTemperature1ID, OBDHParameters::PCBTemperature2ID, OBCSpacecraftTimeRef, OBCOperationalMode, OBCMemoryPartition, OBCReconfigurationTimer, OBCLastFailedEvent, OBCMCUSystick}},
+    //         // OBC Mission Database parameters not sampled
+    //         {2, 5000, false, {OBCMCUInputVoltage, OBCFlashInt, OBCSRAMInt, OBCAvailableMRAM, OBCAvailableNAND, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCMCUFDIR, OBCMCURestartSafeModeThreshold, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCNANDFLASHScrubbingFrequency, OBCRAMScrubbingFrequency, OBCProgramFlashScrubbingFrequency}},
+    //     }};
 } // namespace HousekeepingStructures
 
 #endif
