d3f84068eb667d008bfb2689a3f390578d47067a
Ilia Zarka <iliazark@gmail.com>
2025-02-14 17:31:18 +0200

    chore: added parameter.hpp

--- inc/Platform/Parameters/HousekeepingService.hpp
+++ inc/Platform/Parameters/HousekeepingService.hpp
@@ -5,17 +5,17 @@
 #include "Services/HousekeepingService.hpp"
 
 namespace HousekeepingStructures {
-    using namespace PeakSatParameters;
-
-    static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
-        {
-            // OBC Parameters that will be monitored in the campaign
-            {0, 1000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCMCUTemperature, OBCMCUBootCounter, OBCMemoryPartition, OBCMCUSystick, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCMRAMON, OBCUseCAN, OBCUseUART, OBCUseRTT, OBCOnBoardTime}},
-            // OBC Mission Database parameters monitored every 3 sec
-            {1, 3000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCSpacecraftTimeRef, OBCOperationalMode, OBCMemoryPartition, OBCReconfigurationTimer, OBCLastFailedEvent, OBCMCUSystick}},
-            // OBC Mission Database parameters not sampled
-            {2, 5000, false, {OBCMCUInputVoltage, OBCFlashInt, OBCSRAMInt, OBCAvailableMRAM, OBCAvailableNAND, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCMCUFDIR, OBCMCURestartSafeModeThreshold, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCNANDFLASHScrubbingFrequency, OBCRAMScrubbingFrequency, OBCProgramFlashScrubbingFrequency}},
-        }};
+    // using namespace PeakSatParameters;
+    //
+    // static etl::array<HousekeepingStructure, ECSSMaxHousekeepingStructures> structures = {
+    //     {
+    //         // OBC Parameters that will be monitored in the campaign
+    //         {0, 1000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCMCUTemperature, OBCMCUBootCounter, OBCMemoryPartition, OBCMCUSystick, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCMRAMON, OBCUseCAN, OBCUseUART, OBCUseRTT, OBCOnBoardTime}},
+    //         // OBC Mission Database parameters monitored every 3 sec
+    //         {1, 3000, true, {OBCPCBTemperature1, OBCPCBTemperature2, OBCSpacecraftTimeRef, OBCOperationalMode, OBCMemoryPartition, OBCReconfigurationTimer, OBCLastFailedEvent, OBCMCUSystick}},
+    //         // OBC Mission Database parameters not sampled
+    //         {2, 5000, false, {OBCMCUInputVoltage, OBCFlashInt, OBCSRAMInt, OBCAvailableMRAM, OBCAvailableNAND, OBCCANBUSLoad1, OBCCANBUSLoad2, OBCCANBUSActive, OBCMCUFDIR, OBCMCURestartSafeModeThreshold, OBCNANDFLASHLCLThreshold, OBCMRAMLCLThreshold, OBCNANDFLASHON, OBCNANDFLASHScrubbingFrequency, OBCRAMScrubbingFrequency, OBCProgramFlashScrubbingFrequency}},
+    //     }};
 } // namespace HousekeepingStructures
 
 #endif
